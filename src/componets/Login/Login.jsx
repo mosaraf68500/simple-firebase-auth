a3911f578d36a2974374777119252723e6a77a96
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-26 23:14:06 +0600

    signout

--- src/componets/Login/Login.jsx
+++ src/componets/Login/Login.jsx
@@ -1,41 +1,61 @@
-
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React, { useState } from 'react';
-import { auth } from '../firebase/firebase';
-
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase/firebase";
 
 const Login = () => {
+  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
 
-    const provider=new GoogleAuthProvider();
-    const [user,setUser]=useState(null);
+  const handleGooglebtn = () => {
+    console.log("signIn btn clicked");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        setUser(result.user);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  };
 
-    const handleGooglebtn=()=>{
-        console.log('signIn btn clicked');
-        signInWithPopup(auth,provider)
-        .then(result=>{
-            console.log(result)
-            setUser(result.user)
-        })
-        .then(error=>{
-            console.log(error)
-        })
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="w-1/3 mx-auto">
+      <h1 className="text-xl text-green-500 pt-4 font-semibold text-center">
+        please Login the form:
+      </h1>
 
-
-    }
-    return (
-        <div className='w-1/3 mx-auto'>
-            <h1 className='text-xl text-green-500 pt-4 font-semibold text-center'>please Login the form:</h1>
-            <div className='flex justify-center'><button onClick={handleGooglebtn} className='btn'>SignIn with Google</button></div>
-            {
-                user && <div>
-                <h1>{user.displayName}</h1>
-                <p>{user.email}</p>
-                <img src={user.photoURL} alt="" />
-            </div>
-            }
+      {user ? (
+        <div className="flex justify-center">
+          <button onClick={handleSignOut} className="btn">
+            SignOut
+          </button>
         </div>
-    );
-
+      ) : (
+        <div className="flex justify-center">
+          <button onClick={handleGooglebtn} className="btn">
+            SignIn with Google
+          </button>
+        </div>
+      )}
+      {user && (
+        <div>
+          <h1>{user.displayName}</h1>
+          <p>{user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Login;
